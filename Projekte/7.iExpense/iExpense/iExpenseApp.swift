12f56d1b3651bcca2aac053ff45c4b3793bcdeb1
Phil S. <131772268+smphseis@users.noreply.github.com>
2025-04-08 08:38:20 +0200

    Update Projekte

---
+++ Projekte/7.iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Phil Seißelberg on 06.04.24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
