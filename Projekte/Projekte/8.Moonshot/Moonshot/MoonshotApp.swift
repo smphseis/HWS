c823ade74fd4d71d267285db1dabf0ba188e2b84
Phil S. <131772268+smphseis@users.noreply.github.com>
2025-04-08 08:40:59 +0200

    Update Projekte

---
+++ Projekte/Projekte/8.Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Phil Seißelberg on 11.04.24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
